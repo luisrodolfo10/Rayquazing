b24acc4a9b5552d9a7c1749f0ba0b84bef5590fb
Alejandro Campayo <alejandro.campayo@est.fib.upc.edu>
2024-12-16 18:23:55 +0100

    mandelbulb working

--- src/shapes/mandelbulb.cpp
+++ src/shapes/mandelbulb.cpp
@@ -1,139 +1,96 @@
-// #include "accel.hpp"
-// #include <cmath>
-// #include <lightwave.hpp>
-// #include <memory>
-// #include <vector>
+#include <cmath>
+#include <lightwave.hpp>
+#include <memory>
+#include <vector>
 
-// namespace lightwave {
+namespace lightwave {
 
-// class SquaredPointCloud : public AccelerationStructure {
-//     int n; // Power exponent for the SquaredPointCloud calculation
-//     std::vector<Vector> pointCloud;
+class MandelBulb : public Shape {
+    int n; // Power exponent for the MandelBulb calculation
 
-// public:
-//     SquaredPointCloud(const Properties &properties) {
-//         n           = properties.get<float>("n", 8);
-//         int DIM     = 8;
-//         float scale = 0.5f / (DIM - 1);
-//         // initialise the pointcloud with the mandelbulb
-//         for (int i = 0; i < DIM; i++) {
-//             for (int j = 0; j < DIM; j++) {
-//                 bool edge = false;
-//                 for (int k = 0; k < DIM; k++) {
-//                     // float x     = (i - DIM / 2) * scale;
-//                     // float y     = (j - DIM / 2) * scale;
-//                     // float z     = (k - DIM / 2) * scale;
-//                     // float r     = sqrt(x * x + y * y + z * z);
-//                     // float theta = atan2(sqrt(x * x + y * y), z);
-//                     // float phi   = atan2(y, x);
+public:
+    MandelBulb(const Properties &properties) {
+        n = properties.get<float>("n", 8);
+    }
 
-//                     // Vector zeta = Vector(0, 0, 0);
+    float getRadius(float x, float y, float z) const {
+        return sqrt(x * x + y * y + z * z);
+    }
 
-//                     // int maxiterations = 10;
-//                     // int n             = 8;
-//                     // int iter          = 0;
-//                     // Vector sphere     = Vector(0, 0, 0);
+    float getTheta(float x, float y, float z) const {
+        return atan2(sqrt(x * x + y * y), z);
+    }
 
-//                     // while (true) {
-//                     //     r     = getRadius(sphere[0], sphere[1],
-//                     sphere[2]);
-//                     //     phi   = getPhi(sphere[0], sphere[1], sphere[2]);
-//                     //     theta = getTheta(sphere[0], sphere[1], sphere[2]);
+    float getPhi(float x, float y, float z) const { return atan2(y, z); }
 
-//                     //     float newx = pow(r, n) * sin(theta * n) * cos(phi
-//                     *
-//                     //     n); float newy = pow(r, n) * sin(theta * n) *
-//                     sin(phi
-//                     //     * n); float newz = pow(r, n) * cos(theta * n);
+    bool intersect(const Ray &ray, Intersection &its,
+                   Sampler &rng) const override {
+        const int MAX_STEPS  = 300;
+        const float MAX_DIST = 20.0f;
+        const float Epsilon  = 0.0001f;
 
-//                     //     sphere[0] = newx + x;
-//                     //     sphere[1] = newy + y;
-//                     //     sphere[2] = newz + z;
+        Point currentPos    = ray.origin;
+        float totalDistance = 0.1f;
+        float dist          = std::numeric_limits<float>::max();
+        for (int i = 0; i < MAX_STEPS && totalDistance < MAX_DIST; ++i) {
+            dist = getMandelbulbDistance(currentPos);
+            currentPos += ray.direction * dist;
+            totalDistance += abs(dist);
+            if (dist < Epsilon) {
+                break;
+            }
+        }
+        if (dist < Epsilon) {
+            its.position       = currentPos;
+            its.t              = totalDistance;
+            its.geometryNormal = Vector(currentPos);
+            its.shadingNormal  = Vector(currentPos);
+            return true;
+        }
+        return false;
+    }
 
-//                     //     if (sphere[0] > 5) {
-//                     //         if (edge) {
-//                     //             edge = false;
-//                     //         }
-//                     //         break;
-//                     //     }
+    float getMandelbulbDistance(const Point &p) const {
+        Vector z                = Vector(p);
+        float dr                = 1.0;
+        float r                 = 0.0;
+        const float bailout     = 2.0f;
+        const int maxIterations = 16;
 
-//                     //     iter++;
-//                     //     if (iter > maxiterations) {
-//                     //         if (not edge) {
-//                     //             edge = true;
-//                     pointCloud.push_back(Vector(i, j, k));
-//                     //                         break;
-//                     //                     }
-//                     //                 }
-//                     //             }
-//                 }
-//             }
-//         }
-//         buildAccelerationStructure();
-//     }
+        for (int i = 0; i < maxIterations; i++) {
+            r = getRadius(z[0], z[1], z[2]);
+            if (r > bailout)
+                break;
 
-//     float getRadius(float x, float y, float z) {
-//         return sqrt(x * x + y * y + z * z);
-//     }
+            // Spherical coordinates
+            float theta = getTheta(z[0], z[1], z[2]);
+            float phi   = getPhi(z[0], z[1], z[2]);
 
-//     float getTheta(float x, float y, float z) {
-//         return atan2(sqrt(x * x + y * y), z);
-//     }
+            // Update z
+            float zr = std::pow(r, n);
+            z = Vector(zr * std::sin(theta * n) * std::cos(phi * n) + p[0],
+                       zr * std::sin(theta * n) * std::sin(phi * n) + p[1],
+                       zr * std::cos(theta * n) + p[2]);
 
-//     float getPhi(float x, float y, float z) { return atan2(y, z); }
+            // Update the distance
+            dr = std::pow(r, n - 1) * n * dr + 1.0;
+        }
+        return 0.5f * std::log(r) * r / dr;
+    }
 
-//     bool intersect(int primitiveIndex, const Ray &ray, Intersection &its,
-//                    Sampler &rng) const override {
-//         const Vector &point  = pointCloud[primitiveIndex];
-//         const int MAX_STEPS  = 50;
-//         const float MAX_DIST = 15.0f;
-//         const float Epsilon  = 0.05f;
+    Bounds getBoundingBox() const override {
+        return Bounds(Point(-1, -1, -1), Point(1, 1, 1));
+    }
 
-//         Point currentPos    = ray.origin;
-//         float totalDistance = 0.1f;
+    Point getCentroid() const override { return Point(0, 0, 0); }
 
-//         for (int i = 0; i < MAX_STEPS && totalDistance < MAX_DIST; ++i) {
-//             float dist = (currentPos - point).length;
+    AreaSample sampleArea(Sampler &rng) const override{ NOT_IMPLEMENTED }
 
-//             if (dist < Epsilon) {
-//                 its.position       = currentPos;
-//                 its.t              = totalDistance;
-//                 its.geometryNormal = Vector(currentPos);
-//                 its.shadingNormal  = Vector(currentPos);
-//                 return true;
-//             }
+    std::string toString() const override {
+        return "MandelBulb";
+    }
+};
 
-//             currentPos += ray.direction * dist;
-//             totalDistance += dist;
-//         }
-//         return false;
-//     }
+} // namespace lightwave
 
-//     float getDistance(const Point &p) const {
-//         float minDistance = std::numeric_limits<float>::max();
-
-//         for (const Vector &point : pointCloud) {
-//             float distance = Vector(p - point).length();
-//             if (distance < minDistance) {
-//                 minDistance = distance;
-//             }
-//         }
-//         return minDistance;
-//     }
-
-//     Bounds getBoundingBox() const override {
-//         return Bounds(Point(-1, -1, -1), Point(1, 1, 1));
-//     }
-
-//     Point getCentroid() const override { return Point(0, 0, 0); }
-
-//     AreaSample sampleArea(Sampler &rng) const override{ NOT_IMPLEMENTED }
-
-//     std::string toString() const override {
-//         return "SquaredPointCloud";
-//     }
-// };
-
-// } // namespace lightwave
-
-// REGISTER_SHAPE(SquaredPointCloud, "SquaredPointCloud");
+REGISTER_SHAPE(MandelBulb, "MandelBulb");
