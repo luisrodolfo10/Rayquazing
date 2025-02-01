0454165d9a4c713069a3c0a2fcaf2376e0030a35
Alejandro Campayo <alejandro.campayo@est.fib.upc.edu>
2025-02-01 18:56:40 +0100

    albedo integrator

---
+++ src/integrators/albedo.cpp
@@ -0,0 +1,36 @@
+#include <lightwave.hpp>
+
+namespace lightwave {
+
+class AlbedoIntegrator : public SamplingIntegrator {
+public:
+    AlbedoIntegrator(const Properties &properties)
+        : SamplingIntegrator(properties) {}
+
+    Color Li(const Ray &ray, Sampler &rng) override {
+        Intersection its = m_scene->intersect(ray, rng);
+        if (its) {
+            // Evaluate the BSDF at the intersection to get the albedo.
+            BsdfSample sample = its.sampleBsdf(rng);
+            Color albedo      = sample.weight;
+            return albedo;
+        } else {
+            // If no intersection, return black.
+            return Color(0.0f);
+        }
+    }
+
+    std::string toString() const override {
+        return tfm::format(
+            "AlbedoIntegrator[\n"
+            "  sampler = %s,\n"
+            "  image = %s,\n"
+            "]",
+            indent(m_sampler),
+            indent(m_image));
+    }
+};
+
+} // namespace lightwave
+
+REGISTER_INTEGRATOR(AlbedoIntegrator, "albedo")
